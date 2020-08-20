5ceb257ff15eb1a0c053e64bcd5d76a668a05226
mertsalovda <korranhorn009@gmail.com>
2020-08-21 01:53:33 +0300

    Service

--- app/src/main/java/ru/mertsalovda/audioplayer/services/MediaPlaybackService.kt
+++ app/src/main/java/ru/mertsalovda/audioplayer/services/MediaPlaybackService.kt
@@ -1,10 +1,13 @@
 package ru.mertsalovda.audioplayer.services
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
 import androidx.media.MediaBrowserServiceCompat
+import ru.mertsalovda.audioplayer.MainActivity
 
 private const val MEDIA_ROOT_ID = "media_root_id"
 private const val EMPTY_MEDIA_ROOT_ID = "empty_root_id"
@@ -13,55 +16,62 @@ private const val LOG_TAG = "MediaPlaybackService"
 class MediaPlaybackService : MediaBrowserServiceCompat() {
 
     private var mediaSession: MediaSessionCompat? = null
-    private lateinit var stateBuilder: PlaybackStateCompat.Builder
+    private var stateBuilder = PlaybackStateCompat.Builder()
+        .setActions(
+            PlaybackStateCompat.ACTION_PLAY
+                    or PlaybackStateCompat.ACTION_STOP
+                    or PlaybackStateCompat.ACTION_PAUSE
+                    or PlaybackStateCompat.ACTION_PLAY_PAUSE
+                    or PlaybackStateCompat.ACTION_SKIP_TO_NEXT
+                    or PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
+        )
+    private val mediaSessionCallback = object : MediaSessionCompat.Callback() {
+        override fun onPlay() {
+        }
+        override fun onPause() {
+        }
+        override fun onStop() {
+        }
+        override fun onSkipToNext() {
+        }
+        override fun onSkipToPrevious() {
+        }
+    }
 
     override fun onCreate() {
         super.onCreate()
+        val activityIntent = Intent(applicationContext, MainActivity::class.java);
 
-        // Create a MediaSessionCompat
         mediaSession = MediaSessionCompat(baseContext, LOG_TAG).apply {
-
-            // Enable callbacks from MediaButtons and TransportControls
             setFlags(
-                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS or MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS
+                        or MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS
             )
-
-            // Set an initial PlaybackState with ACTION_PLAY, so media buttons can start the player
-            stateBuilder = PlaybackStateCompat.Builder()
-                .setActions(
-                    PlaybackStateCompat.ACTION_PLAY or PlaybackStateCompat.ACTION_PLAY_PAUSE
-                )
             setPlaybackState(stateBuilder.build())
-
-            // MySessionCallback() has methods that handle callbacks from a media controller
-            setCallback(MySessionCallback())
-
-            // Set the session's token so that client activities can communicate with it.
+            setCallback(mediaSessionCallback)
             setSessionToken(sessionToken)
+            setSessionActivity(
+                PendingIntent.getActivity(applicationContext, 0, activityIntent, 0))
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaSession?.release()
     }
 
     override fun onLoadChildren(
         parentId: String,
         result: Result<MutableList<MediaBrowserCompat.MediaItem>>
     ) {
-        //  Browsing not allowed
-        if (EMPTY_MEDIA_ROOT_ID == parentMediaId) {
+        if (EMPTY_MEDIA_ROOT_ID == parentId) {
             result.sendResult(null)
             return
         }
+        val mediaItems = mutableListOf<MediaBrowserCompat.MediaItem>()
 
-        // Assume for example that the music catalog is already loaded/cached.
-
-        val mediaItems = emptyList<MediaBrowserCompat.MediaItem>()
-
-        // Check if this is the root menu:
-        if (MEDIA_ROOT_ID == parentMediaId) {
-            // Build the MediaItem objects for the top level,
-            // and put them in the mediaItems list...
+        if (MEDIA_ROOT_ID == parentId) {
         } else {
-            // Examine the passed parentMediaId to see which submenu we're at,
-            // and put the children of that menu in the mediaItems list...
         }
         result.sendResult(mediaItems)
     }
